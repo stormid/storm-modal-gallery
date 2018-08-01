725b56a252cedc746899ad57d1ca218ff1367666
mjbp <mick.perkins@stormid.com>
2018-08-01 15:35:46 +0100

    Update 2018-08-01T14:35:41.170Z

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /**
  * @name storm-modal-gallery: Modal gallery/lightbox
- * @version 1.3.2: Fri, 09 Mar 2018 17:26:42 GMT
+ * @version 1.3.4: Wed, 01 Aug 2018 14:33:48 GMT
  * @author mjbp
  * @license MIT
  */
